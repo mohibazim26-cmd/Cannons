ab8c1b761ccdfe118e7ad7cc7ba092f98cb4a413
mohibazim26-cmd <mohibazim26@gmail.com>
2026-06-01 21:28:32 +0200

    Update cannon retrieval method in RotationListener

--- cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/movecraft/listener/RotationListener.java
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/movecraft/listener/RotationListener.java
@@ -17,10 +17,14 @@ public class RotationListener implements Listener {
     public void rotateListener(CraftRotateEvent e) {
         Craft craft = e.getCraft();
 
-        Set<Cannon> cannons = MovecraftUtils.getCannons(craft);
+        Set<Cannon> cannons = MovecraftUtils.getRegisteredCannons(craft);
+        if (cannons.isEmpty()) {
+            cannons = MovecraftUtils.getCannons(craft.getWorld(), e.getOldHitBox());
+        }
         if (cannons.isEmpty())
             return;
 
+        MovecraftUtils.registerCannons(craft, cannons);
         Vector v = e.getOriginPoint().toBukkit(craft.getWorld()).toVector();
         for (Cannon c : cannons) {
             switch (e.getRotation()) {
