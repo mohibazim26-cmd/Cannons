3b803fdee29cf1236e11abe384aa682f4f869f0c
mohibazim26-cmd <mohibazim26@gmail.com>
2026-06-02 21:16:04 +0200

    Add new NamespacedKeys for cannon and torpedo firepower

--- cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/movecraft/type/CraftKeys.java
+++ cannons-bukkit/src/main/java/at/pavlov/cannons/hooks/movecraft/type/CraftKeys.java
@@ -13,6 +13,10 @@ public class CraftKeys {
     public static final NamespacedKey MAX_MASS = Cannons.nsKey("max_mass");
     public static final NamespacedKey MIN_MASS = Cannons.nsKey("min_mass");
     public static final NamespacedKey EXCLUDE_FROM_MASS = Cannons.nsKey("exclude_from_mass");
+    public static final NamespacedKey MAX_CANNON_FIREPOWER = Cannons.nsKey("max_cannon_firepower");
+    public static final NamespacedKey MAX_TORPEDO_FIREPOWER = Cannons.nsKey("max_torpedo_firepower");
+    public static final NamespacedKey TORPEDO_OVERFLOW_USES_CANNON_FIREPOWER = Cannons.nsKey("torpedo_overflow_uses_cannon_firepower");
+    public static final NamespacedKey SCALE_FIREPOWER_WITH_SIZE = Cannons.nsKey("scale_firepower_with_size");
 
     public static final NamespacedKey USE_SHIP_ANGLES = Cannons.nsKey("use_ship_angles");
 }
